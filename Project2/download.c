e6538d41ea823ecb4a4998ea78e7c6c808173ad3
Duarte Carvalho <up201503661@fe.up.pt>
2018-11-24 16:17:16 +0000

    printusage

--- Project2/download.c
+++ Project2/download.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+#define clear() printf("\033[H\033[J")
+
+void printUsage()
+{
+  clear();
+
+  printf("Usage:");
+  printf("\n\t./download ftp://<user>:<password>@<host>:<port>/<url-path>");
+  printf("\nOR");
+  printf("\n\t./download ftp://<host>/<url-path>\n");
+}
+
 int main()
 {
+  printUsage();
   return 0;
 }
