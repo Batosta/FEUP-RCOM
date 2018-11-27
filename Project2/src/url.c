2f851c5a2d10e6ea8fdea16cd59bf872bf76b274
Duarte Carvalho <up201503661@fe.up.pt>
2018-11-27 16:01:42 +0000

    user authentication parse done

--- Project2/src/url.c
+++ Project2/src/url.c
@@ -74,11 +74,40 @@ int validURL(char *insertedURL)
 
 int parseURL(int Mode, url *link, char *inserted)
 {
+  char *username, *password, *host, *portStr, *path;
+
+  int usernameStart, usernameEnd, pwEnd, hostEnd, portEnd, length;
+
+  length = strlen(inserted);
+
   if (Mode == USERAUTH)
   {
-    int usernameStart = findOcorrenceIndex(inserted, '[', 0) + 1;
-    // printf("index= %d\n", findOcorrenceIndex(inserted, '[', 0));
-    // printf("index= %d\n", findOcorrenceIndex(inserted, ':', 3));
+    usernameStart = findOcorrenceIndex(inserted, '[', 0) + 1;
+    usernameEnd = findOcorrenceIndex(inserted, ':', usernameStart);
+    username = (char *)malloc(usernameEnd - usernameStart);
+    memcpy(username, inserted + usernameStart, usernameEnd - usernameStart);
+
+    pwEnd = findOcorrenceIndex(inserted, '@', usernameEnd + 1);
+    password = (char *)malloc(pwEnd - (usernameEnd + 1));
+    memcpy(password, inserted + usernameEnd + 1, pwEnd - (usernameEnd + 1));
+
+    hostEnd = findOcorrenceIndex(inserted, ':', pwEnd + 1);
+    host = (char *)malloc(hostEnd - (pwEnd + 2));
+    memcpy(host, inserted + pwEnd + 2, hostEnd - (pwEnd + 2));
+
+    portEnd = findOcorrenceIndex(inserted, '/', hostEnd + 1);
+    portStr = (char *)malloc(portEnd - (hostEnd + 1));
+    memcpy(portStr, inserted + hostEnd + 1, portEnd - (hostEnd + 1));
+
+    path = (char *)malloc(length - (portEnd + 1));
+    memcpy(path, inserted + portEnd + 1, length - (portEnd + 1));
+
+    printf("\nUsername: %s \n", username);
+    printf("Password: %s \n", password);
+    printf("Host: %s \n", host);
+    printf("Port: %s \n", portStr);
+    printf("Path: %s \n", path);
+
     return 0;
   }
   else if (Mode == ANONIMOUS)
